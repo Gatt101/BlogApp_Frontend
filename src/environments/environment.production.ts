f536e62d9d540a9470b73bf8a4a5d59a64c6423f
Gatt101 <gauravpatilk11@gmail.com>
2025-03-09 16:21:43 +0530

    dest

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://blogapp-backend-vl52.onrender.com' // Will be replaced at runtime
+    apiUrl: 'https://blogapp-backend-vl52.onrender.com/api/post' // Will be replaced at runtime
   };
   
